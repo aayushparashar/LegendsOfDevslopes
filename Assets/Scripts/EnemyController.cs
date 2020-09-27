cd9dcfc6ee2e058abdd3ec48279906dee23cbb1b
Aayush <ayushparashar14@gmail.com>
2020-09-27 14:25:00 +0530

    Legends Of Devslopes

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyController : MonoBehaviour
+{
+    private Transform player;
+    private NavMeshAgent nav;
+    private Animator anim;
+    EnemyHealth enemy;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = (GameManager.instance.Player as GameObject).transform;
+        enemy = GetComponent<EnemyHealth>();
+        nav = GetComponent<NavMeshAgent>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (enemy.IsDead)
+        {
+            nav.enabled = false;
+        }else 
+        if (GameManager.instance.GameOver)
+        {
+            nav.enabled = false;
+            anim.Play("Idle");
+        }
+        else
+        {
+            nav.SetDestination(player.position);
+        }
+        
+    }
+}
